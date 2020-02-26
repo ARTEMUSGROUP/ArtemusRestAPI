f3ef93a274b4b5bef0d92c050e776717bf3156d7
Radhika Udapure <radhika@giantleapsystems.com>
2020-02-26 16:16:47 +0530

    Done Response change in Voyage

--- ArtemusRestApi/src/main/java/com/artemus/app/VoyagesEntryPoint.java
+++ ArtemusRestApi/src/main/java/com/artemus/app/VoyagesEntryPoint.java
@@ -12,21 +12,27 @@ import javax.ws.rs.core.Response;
 
 import com.artemus.app.annotations.Secured;
 import com.artemus.app.model.request.Voyage;
+import com.artemus.app.model.response.ResponseMessage;
 import com.artemus.app.service.VoyageScheduleService;
 import com.artemus.app.service.impl.VoyageScheduleServiceImpl;
 
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces({ MediaType.APPLICATION_JSON})
+@Produces({ MediaType.APPLICATION_JSON })
 @Path("voyages")
 public class VoyagesEntryPoint {
 
 	@Secured
 	@POST
-	public Response createVoyage(Voyage objRequest,@HeaderParam("Authorization") String authorization) {
-		String scacCode = authorization.substring(0,4)  ;
+	public ResponseMessage createVoyage(Voyage objRequest, @HeaderParam("Authorization") String authorization) {
+		String scacCode = authorization.substring(0, 4);
 		objRequest.setScacCode(scacCode);
 		VoyageScheduleService objService = new VoyageScheduleServiceImpl();
 		objService.createVoyage(objRequest);
-		return Response.status(200).entity("SUCCESS").build();
+		
+		ResponseMessage objResponse = new ResponseMessage();
+		objResponse.setCode(200);
+		objResponse.setStatus(Response.Status.OK);
+		objResponse.setMessage("Voyage Created...");
+		return objResponse;
 	}
 }
