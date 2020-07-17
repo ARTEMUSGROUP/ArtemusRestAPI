dd999208ac242908d507345d4710085c570aa265
Akshay <chetantikone00@gmail.com>
2020-07-17 15:26:31 +0530

    Validation in Japan for Location atleast one load/discharge Port Done.

--- ArtemusRestApi/src/main/java/com/artemus/app/service/impl/JPVoyageScheduleServiceImpl.java
+++ ArtemusRestApi/src/main/java/com/artemus/app/service/impl/JPVoyageScheduleServiceImpl.java
@@ -142,7 +142,7 @@ public class JPVoyageScheduleServiceImpl implements JPVoyageScheduleService {
 								break;
 							}
 						}
-					}else {
+					} else {
 						if ((locationbean.getLocation() == null || locationbean.getLocation().isEmpty())) {
 							if (errorMessage.length() > 0) {
 								errorMessage.append(" , ");
@@ -288,6 +288,26 @@ public class JPVoyageScheduleServiceImpl implements JPVoyageScheduleService {
 				result = false;
 			}
 
+			// Location enter must be loadport or discharge port
+			portValidation = false;
+			String locationName = "";
+			for (PortDetails portCall : objVoyage.getPortDetails()) {
+				if (portCall.getLoad() != true && portCall.getDischarge() != true) {
+					locationName = portCall.getLocation().getLocation();
+					portValidation = true;
+					break;
+				}
+			}
+
+			if (portValidation) {
+				if (errorMessage.length() > 0) {
+					errorMessage.append(" , ");
+				}
+				errorMessage.append(
+						"Location entered:" + locationName + " must be assigned as a Load Port or Dischage Port  ");
+				result = false;
+			}
+
 			// Validate first discharge port as japan port
 			Date jpArrivalDate[] = new Date[dischargeportCount];
 			Date firstDischargedate;
@@ -378,15 +398,26 @@ public class JPVoyageScheduleServiceImpl implements JPVoyageScheduleService {
 						result = false;
 						break;
 					}
-					if (dichargedate1.compareTo(lastloaddate1) >= 0 || dichargedate1.compareTo(lastloaddate1) == 0) {
-						result = true;
+					if (lastloaddate1 != null) {
+						if (dichargedate1.compareTo(lastloaddate1) >= 0
+								|| dichargedate1.compareTo(lastloaddate1) == 0) {
+							result = true;
+						} else {
+							if (errorMessage.length() > 0) {
+								errorMessage.append(" , ");
+							}
+							// Error message
+							errorMessage
+									.append(" 'arrivalDate' of discharge port " + portCall.getLocation().getLocation()
+											+ "should not be less than last load port please check 'portDetails' ");
+						}
 					} else {
 						if (errorMessage.length() > 0) {
 							errorMessage.append(" , ");
 						}
 						// Error message
-						errorMessage.append(" 'arrivalDate' of discharge port " + portCall.getLocation().getLocation()
-								+ "should not be less than last load port please check 'portDetails' ");
+						errorMessage.append("lastLoadPort : Voyage should have atleast one lastLoadPort");
+						break;
 					}
 				}
 			}
@@ -454,7 +485,7 @@ public class JPVoyageScheduleServiceImpl implements JPVoyageScheduleService {
 		boolean result = true;
 		try {
 			for (PortDetails objPortDetail : objVoyage.getPortDetails()) {
-				String customCodefromUNCode="";
+				String customCodefromUNCode = "";
 				Location locationbean = objPortDetail.getLocation();
 				objLocationdao.setLocationBean(locationbean);
 				if (locationbean.getLocationType().equalsIgnoreCase("I")) {
@@ -475,18 +506,18 @@ public class JPVoyageScheduleServiceImpl implements JPVoyageScheduleService {
 					int locationIdfromUNCode = objLocationdao.getLocationIdfromUnlocode(locationbean.getCustomCode(),
 							loginScac);
 					int locationIdfromLocation = objLocationdao.getLocationId(locationbean.getLocation(), loginScac);
-					if(locationIdfromLocation!=0 && locationIdfromUNCode !=0) {
+					if (locationIdfromLocation != 0 && locationIdfromUNCode != 0) {
 						if (locationIdfromUNCode != locationIdfromLocation) {
-							errorMessage.append("Unlocode: " + locationbean.getUnlocode()
-							+ "for Custom Code"+locationbean.getCustomCode() +"entered is same for multiple Locations in AMS system.");
+							errorMessage.append("Unlocode: " + locationbean.getUnlocode() + "for Custom Code"
+									+ locationbean.getCustomCode()
+									+ "entered is same for multiple Locations in AMS system.");
 						}
 					}
-					
+
 					// Insert into location
-					if(locationIdfromLocation==0 && locationIdfromUNCode==0) {
-							objLocationdao.insert(locationbean, loginScac);
+					if (locationIdfromLocation == 0 && locationIdfromUNCode == 0) {
+						objLocationdao.insert(locationbean, loginScac);
 					}
-					
 
 					if (objLocationdao.isForeignPort(locationbean.getCustomCode())) {
 						locationbean.setCustomForeign(true);
@@ -512,7 +543,8 @@ public class JPVoyageScheduleServiceImpl implements JPVoyageScheduleService {
 						errorMessage.append(" , ");
 					}
 					if (locationbean.getUnlocode() != null && (!locationbean.getUnlocode().isEmpty())) {
-						errorMessage.append("customCode for unlocode : " + locationbean.getUnlocode()+"is invalid or  does not exists for the login scac" + loginScac);
+						errorMessage.append("customCode for unlocode : " + locationbean.getUnlocode()
+								+ "is invalid or  does not exists for the login scac" + loginScac);
 					}
 				}
 			}
